4a20874005cb19cc8e7cd7382cbe47bb30cf62ce
yuchanleeme <yuchanlee.me@gmail.com>
2021-06-23 23:10:36 +0900

    [2-4] Lombok 적용 및 사용

--- src/main/java/com/awspractice/springboot/web/HelloController.java
+++ src/main/java/com/awspractice/springboot/web/HelloController.java
@@ -1,13 +1,23 @@
 package com.awspractice.springboot.web;
 
+import com.awspractice.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    // "/hello"로 get 요청 들어오면 hello 리턴
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    // "/hello/dto"로 get 요청 들어오면 HelloResponseDto 리턴
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
---
+++ src/test/java/com/awspractice/springboot/web/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package com.awspractice.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        // "/hello"로 get 요청 후 테스트
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        // "/hello/dto"로 get 요청 후 테스트
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
