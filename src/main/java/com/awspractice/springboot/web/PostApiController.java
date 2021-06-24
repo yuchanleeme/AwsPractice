3ae67513975e955c4533ed84a72b58245f1c86f2
yuchanleeme <yuchanlee.me@gmail.com>
2021-06-24 22:24:00 +0900

    [3-4] 등록/수정/조회 API 만들기

---
+++ src/main/java/com/awspractice/springboot/web/PostApiController.java
@@ -0,0 +1,31 @@
+package com.awspractice.springboot.web;
+
+import com.awspractice.springboot.service.posts.PostsService;
+import com.awspractice.springboot.web.dto.PostsResponseDto;
+import com.awspractice.springboot.web.dto.PostsSaveRequestDto;
+import com.awspractice.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,
+                       @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
