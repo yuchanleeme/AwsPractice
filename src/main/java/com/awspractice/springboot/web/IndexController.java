1e97d9dea1ff53671c80acde6515e46c0259e0ce
yuchanleeme <yuchanlee.me@gmail.com>
2021-06-25 16:32:45 +0900

    [4-5] 게시글 수정/삭제 기능 만들기

--- src/main/java/com/awspractice/springboot/web/IndexController.java
+++ src/main/java/com/awspractice/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.awspractice.springboot.web;
 
 import com.awspractice.springboot.service.posts.PostsService;
+import com.awspractice.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "post-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
